7aec9724a74d0766592c0729a5ca0070db869c58
Netzchhh <netzchhh@mail.ru>
2025-11-07 15:44:39 +0300

    Update octagon.cpp

--- src/tests/octagon.cpp
+++ src/tests/octagon.cpp
@@ -25,17 +25,14 @@ TEST(OctagonTest, CopyConstructor) {
     Point<int> point1{0, 0}, point2{1, 0}, point3{2, 1}, point4{3, 2}, point5{2, 3}, point6{1, 4}, point7{0, 4}, point8{-1, 3};
     Octagon<int> octagon1(point1, point2, point3, point4, point5, point6, point7, point8);
     Octagon<int> octagon2(octagon1);
-
     EXPECT_EQ(octagon1.getSize(), octagon2.getSize());
     EXPECT_TRUE(octagon1 == octagon2);
 }
 
 TEST(OctagonTest, GetCenter) {
-    Octagon<double> octagon(Point<double>(0, 0), Point<double>(1, 0), Point<double>(2, 1),
-                            Point<double>(3, 2), Point<double>(2, 3), Point<double>(1, 4),
-                            Point<double>(0, 4), Point<double>(-1, 3));
+    Octagon<double> octagon(Point<double>(0, 0), Point<double>(1, 0), Point<double>(2, 1), Point<double>(3, 2),
+                    Point<double>(2, 3), Point<double>(1, 4), Point<double>(0, 4), Point<double>(-1, 3));
     Point<double> center = octagon.getCenter();
-
     EXPECT_DOUBLE_EQ(center.x, 1.0);
     EXPECT_DOUBLE_EQ(center.y, 2.125);
 }
@@ -43,7 +40,6 @@ TEST(OctagonTest, GetCenter) {
 TEST(OctagonTest, GetArea) {
     Octagon<int> octagon(Point<int>(0, 0), Point<int>(1, 0), Point<int>(2, 1), Point<int>(3, 2),
                          Point<int>(2, 3), Point<int>(1, 4), Point<int>(0, 4), Point<int>(-1, 3));
-
     double area = octagon.getArea();
     EXPECT_NEAR(area, 10.0, 1e-6);
 }
@@ -52,20 +48,16 @@ TEST(OctagonTest, Print) {
     Octagon<int> octagon(Point<int>(0, 0), Point<int>(1, 0), Point<int>(2, 1), Point<int>(1, 2),
                          Point<int>(0, 2), Point<int>(-1, 2), Point<int>(-1, 1), Point<int>(0, 1));
     ostringstream os;
-
     octagon.print(os);
     string output = os.str();
-
     EXPECT_NE(output.find("Area"), string::npos);
     EXPECT_NE(output.find("(0,0)"), string::npos);
 }
 
 TEST(OctagonTest, Read) {
     Octagon<int> octagon;
-
     istringstream is("0 0 1 0 2 1 3 2 2 3 1 4 0 4 -1 3");
     octagon.read(is);
-
     Point<int> point1{0, 0}, point2{1, 0}, point3{2, 1}, point4{3, 2}, point5{2, 3}, point6{1, 4}, point7{0, 4}, point8{-1, 3};
     const Point<int>* points = octagon.getPoints();
     EXPECT_EQ(points[0], point1);
@@ -84,7 +76,6 @@ TEST(OctagonTest, OperatorEquals) {
     Octagon<int> octagon2(octagon1);
     Octagon<int> octagon3(Point<int>(1, 1), Point<int>(2, 1), Point<int>(3, 2), Point<int>(4, 3),
                           Point<int>(3, 4), Point<int>(2, 5), Point<int>(1, 5), Point<int>(0, 4));
-
     EXPECT_TRUE(octagon1 == octagon2);
     EXPECT_FALSE(octagon1 == octagon3);
 }
@@ -92,7 +83,6 @@ TEST(OctagonTest, OperatorEquals) {
 TEST(OctagonTest, Clone) {
     Octagon<int> octagon1(Point<int>(0, 0), Point<int>(1, 0), Point<int>(2, 1), Point<int>(3, 2),
                           Point<int>(2, 3), Point<int>(1, 4), Point<int>(0, 4), Point<int>(-1, 3));
-
     auto octagon2 = octagon1.clone();
     EXPECT_TRUE(dynamic_cast<Octagon<int>*>(octagon2.get()) != nullptr);
     EXPECT_TRUE(octagon1 == *octagon2);
