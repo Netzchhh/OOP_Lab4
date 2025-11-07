f3715762202501d7a47ad71dfa6e731612be5f45
Netzchhh <netzchhh@mail.ru>
2025-11-07 15:46:05 +0300

    Update triangle.cpp

--- src/tests/triangle.cpp
+++ src/tests/triangle.cpp
@@ -12,9 +12,7 @@ TEST(TriangleTest, DefaultConstructor) {
 TEST(TriangleTest, ParameterConstructor) {
     Point<int> point1{0, 0}, point2{2, 0}, point3{1, 2};
     Triangle<int> triangle(point1, point2, point3);
-
     EXPECT_EQ(triangle.getSize(), 3);
-
     const Point<int>* points = triangle.getPoints();
     EXPECT_EQ(points[0], point1);
     EXPECT_EQ(points[1], point2);
@@ -25,7 +23,6 @@ TEST(TriangleTest, CopyConstructor) {
     Point<int> point1{0, 0}, point2{2, 0}, point3{1, 2};
     Triangle<int> triangle1(point1, point2, point3);
     Triangle<int> triangle2(triangle1);
-
     EXPECT_EQ(triangle1.getSize(), triangle2.getSize());
     EXPECT_TRUE(triangle1 == triangle2);
 }
@@ -33,14 +30,12 @@ TEST(TriangleTest, CopyConstructor) {
 TEST(TriangleTest, GetCenter) {
     Triangle<double> triangle(Point<double>(0, 0), Point<double>(2, 0), Point<double>(1, 2));
     Point<double> center = triangle.getCenter();
-
     EXPECT_DOUBLE_EQ(center.x, (0 + 2 + 1) / 3.0);
     EXPECT_DOUBLE_EQ(center.y, (0 + 0 + 2) / 3.0);
 }
 
 TEST(TriangleTest, GetArea) {
     Triangle<int> triangle(Point<int>(0, 0), Point<int>(2, 0), Point<int>(1, 2));
-
     double area = triangle.getArea();
     EXPECT_NEAR(area, 2.0, 1e-6);
 }
@@ -48,20 +43,16 @@ TEST(TriangleTest, GetArea) {
 TEST(TriangleTest, Print) {
     Triangle<int> triangle(Point<int>(0, 0), Point<int>(1, 0), Point<int>(0, 1));
     ostringstream os;
-
     triangle.print(os);
     string output = os.str();
-
     EXPECT_NE(output.find("Area"), string::npos);
     EXPECT_NE(output.find("(0,0)"), string::npos);
 }
 
 TEST(TriangleTest, Read) {
     Triangle<int> triangle;
-
     istringstream is("0 0 2 0 1 2");
     triangle.read(is);
-
     Point<int> point1{0, 0}, point2{2, 0}, point3{1, 2};
     const Point<int>* points = triangle.getPoints();
     EXPECT_EQ(points[0], point1);
@@ -80,7 +71,6 @@ TEST(TriangleTest, OperatorEquals) {
 
 TEST(TriangleTest, Clone) {
     Triangle<int> triangle1(Point<int>(0, 0), Point<int>(2, 0), Point<int>(1, 2));
-
     auto triangle2 = triangle1.clone();
     EXPECT_TRUE(dynamic_cast<Triangle<int>*>(triangle2.get()) != nullptr);
     EXPECT_TRUE(triangle1 == *triangle2);
