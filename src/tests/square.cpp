7bcc1753ad025423452f2f918bdf00571b6841a6
Netzchhh <netzchhh@mail.ru>
2025-11-07 15:45:38 +0300

    Update square.cpp

--- src/tests/square.cpp
+++ src/tests/square.cpp
@@ -12,9 +12,7 @@ TEST(SquareTest, DefaultConstructor) {
 TEST(SquareTest, ParameterConstructor) {
     Point<int> point1{0, 0}, point2{2, 0}, point3{2, 2}, point4{0, 2};
     Square<int> square(point1, point2, point3, point4);
-
     EXPECT_EQ(square.getSize(), 4);
-
     const Point<int>* points = square.getPoints();
     EXPECT_EQ(points[0], point1);
     EXPECT_EQ(points[1], point2);
@@ -26,7 +24,6 @@ TEST(SquareTest, CopyConstructor) {
     Point<int> point1{0, 0}, point2{2, 0}, point3{2, 2}, point4{0, 2};
     Square<int> square1(point1, point2, point3, point4);
     Square<int> square2(square1);
-
     EXPECT_EQ(square1.getSize(), square2.getSize());
     EXPECT_TRUE(square1 == square2);
 }
@@ -35,7 +32,6 @@ TEST(SquareTest, GetCenter) {
     Square<double> square(Point<double>(0, 0), Point<double>(2, 0), Point<double>(2, 2),
                           Point<double>(0, 2));
     Point<double> center = square.getCenter();
-
     EXPECT_DOUBLE_EQ(center.x, (0 + 2 + 2 + 0) / 4.0);
     EXPECT_DOUBLE_EQ(center.y, (0 + 0 + 2 + 2) / 4.0);
 }
@@ -43,7 +39,6 @@ TEST(SquareTest, GetCenter) {
 TEST(SquareTest, GetArea) {
     Square<int> square(Point<int>(0, 0), Point<int>(2, 0), Point<int>(2, 2),
                        Point<int>(0, 2));
-
     double area = square.getArea();
     EXPECT_NEAR(area, 4.0, 1e-6);
 }
@@ -51,20 +46,16 @@ TEST(SquareTest, GetArea) {
 TEST(SquareTest, Print) {
     Square<int> square(Point<int>(0, 0), Point<int>(1, 0), Point<int>(1, 1), Point<int>(0, 1));
     ostringstream os;
-
     square.print(os);
     string output = os.str();
-
     EXPECT_NE(output.find("Area"), string::npos);
     EXPECT_NE(output.find("(0,0)"), string::npos);
 }
 
 TEST(SquareTest, Read) {
     Square<int> square;
-
     istringstream is("0 0 2 0 2 2 0 2");
     square.read(is);
-
     Point<int> point1{0, 0}, point2{2, 0}, point3{2, 2}, point4{0, 2};
     const Point<int>* points = square.getPoints();
     EXPECT_EQ(points[0], point1);
@@ -79,7 +70,6 @@ TEST(SquareTest, OperatorEquals) {
     Square<int> square2(square1);
     Square<int> square3(Point<int>(1, 1), Point<int>(3, 1), Point<int>(3, 3),
                         Point<int>(1, 3));
-
     EXPECT_TRUE(square1 == square2);
     EXPECT_FALSE(square1 == square3);
 }
@@ -87,7 +77,6 @@ TEST(SquareTest, OperatorEquals) {
 TEST(SquareTest, Clone) {
     Square<int> square1(Point<int>(0, 0), Point<int>(2, 0), Point<int>(2, 2),
                         Point<int>(0, 2));
-
     auto square2 = square1.clone();
     EXPECT_TRUE(dynamic_cast<Square<int>*>(square2.get()) != nullptr);
     EXPECT_TRUE(square1 == *square2);
